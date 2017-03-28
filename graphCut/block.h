c5ead8ae7c4eb39a8846cd68d660efdf9e345070
Dun <randonlang@gmail.com>
2017-03-28 20:43:35 +0800

    char* -> const char*

--- graphCut/block.h
+++ graphCut/block.h
@@ -104,7 +104,7 @@ public:
 	   (optionally) the pointer to the function which
 	   will be called if allocation failed; the message
 	   passed to this function is "Not enough memory!" */
-	Block(int size, void (*err_function)(char *) = NULL) { first = last = NULL; block_size = size; error_function = err_function; }
+	Block(int size, void (*err_function)(const char *) = NULL) { first = last = NULL; block_size = size; error_function = err_function; }
 
 	/* Destructor. Deallocates all items added so far */
 	~Block() { while (first) { block *next = first -> next; delete[] ((char*)first); first = next; } }
@@ -193,7 +193,7 @@ private:
 	block	*scan_current_block;
 	Type	*scan_current_data;
 
-	void	(*error_function)(char *);
+    void	(*error_function)(const char *);
 };
 
 /***********************************************************************/
@@ -207,7 +207,7 @@ public:
 	   (optionally) the pointer to the function which
 	   will be called if allocation failed; the message
 	   passed to this function is "Not enough memory!" */
-	DBlock(int size, void (*err_function)(char *) = NULL) { first = NULL; first_free = NULL; block_size = size; error_function = err_function; }
+	DBlock(int size, void (*err_function)(const char *) = NULL) { first = NULL; first_free = NULL; block_size = size; error_function = err_function; }
 
 	/* Destructor. Deallocates all items added so far */
 	~DBlock() { while (first) { block *next = first -> next; delete[] ((char*)first); first = next; } }
@@ -261,7 +261,7 @@ private:
 	block		*first;
 	block_item	*first_free;
 
-	void	(*error_function)(char *);
+    void	(*error_function)(const char *);
 };
 
 
